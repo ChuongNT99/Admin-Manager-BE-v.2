52cd75eadff674d27f08b4ce1dd961bb27e02421
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-11-05 16:12:57 +0700

    update

--- main/controller/__init__.py
+++ main/controller/__init__.py
@@ -1,5 +1,9 @@
 from main.controller import login_controller
 
-from main.controller import manage_room_api
+from main.controller import room_controller
 
 from main.controller import employee_controller
+
+from main.controller import booking_controller
+
+from main.controller import scheduler
