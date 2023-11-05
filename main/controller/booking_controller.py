a52cf3cc36af3df6fe3828aeb08b09dce27069a2
THUANNV <thuannv_tts@rikkeisoft.com>
2023-11-05 15:23:19 +0700

    update booking

--- main/controller/booking_controller.py
+++ main/controller/booking_controller.py
@@ -73,6 +73,59 @@ def book_room():
     else:
         return jsonify({'error': 'Permission denied'}), 403
 
+@app.route("/bookings/<int:booking_id>", methods=["PUT"])
+@jwt_required()
+def update_booking(booking_id):
+    current_user = get_jwt_identity()
+    if current_user == 1:
+        data = request.get_json()
+        room_id = data.get('room_id')
+        time_start = data.get('time_start')
+        time_end = data.get('time_end')
+        employee_ids = data.get('employee_id')
+
+        if time_start is not None and time_end is not None and employee_ids is not None:
+            if time_start <= time_end:
+                return jsonify({'error': 'invalid time input '}), 400
+            existing_booking = Booking.query.filter(
+                Booking.room_id == room_id,
+                Booking.time_end >= time_start,
+                Booking.time_start <= time_end
+            ).first()
+
+            if existing_booking and existing_booking.booking_id != booking_id:
+                return jsonify({'error': 'Room is already booked for this time'}), 400
+
+            try:
+                booking = Booking.query.get(booking_id)
+
+                if booking is None:
+                    return jsonify({'error': 'Booking not found'}), 404
+
+                booking.room_id = room_id
+                booking.time_start = time_start
+                booking.time_end = time_end
+
+                # Xóa tất cả các nhân viên từ cuộc họp
+                for employee_booking in booking.booking_employees:
+                    db.session.delete(employee_booking)
+
+                # Thêm danh sách các nhân viên vào cuộc họp
+                for employee_id in employee_ids:
+                    employee_booking = BookingEmployee(
+                        employee_id=employee_id, booking_id=booking.booking_id)
+                    db.session.add(employee_booking)
+
+                db.session.commit()
+                return jsonify({'message': 'Booking updated successfully'})
+            except Exception as e:
+                db.session.rollback()
+                return jsonify({'error': 'Internal Server Error'}), 500
+        else:
+            return jsonify({'error': 'Invalid time input'}), 400
+    else:
+        return jsonify({'error': 'Permission denied'}), 403
+    
 
 @app.route("/bookings/<int:booking_id>", methods=["DELETE"])
 @jwt_required()
