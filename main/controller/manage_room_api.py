5c2f633fbdcfcfa4906fc1f4eae1bb1bc947504e
CHUONGNT99 <chuongnt_tts@rikkeisoft.com>
2023-11-05 12:36:19 +0700

    manage room

--- main/controller/manage_room_api.py
+++ main/controller/manage_room_api.py
@@ -9,109 +9,82 @@ from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 @jwt_required()
 def get_rooms():
     current_user = get_jwt_identity()
-    print(current_user, "--------------------------------------------------")
     if current_user == 1:
-        #     rooms = Room.query.all()
-        #     room_list = []
-        #     for room in rooms:
-        #         room_data = {
-        #             'room_id': room.room_id,
-        #             'room_name': room.room_name,
-        #             'status': room.status
-        #         }
-        #         room_list.append(room_data)
-        #     return jsonify(rooms=room_list)
-        # return jsonify(message="Permission denied"), 403
-        rooms = Room.query.all()``
+        rooms = Room.query.all()
         return jsonify({"rooms": [room.serialize() for room in rooms]})
     else:
         return jsonify({"error": "Internal Server Error"}), 500
 
 
-# @app.route("/rooms", methods=["POST"])
-# @jwt_required()
-# def create_room():
-#     current_user = get_jwt_identity()
+@app.route("/rooms", methods=["POST"])
+@jwt_required()
+def create_room():
+    current_user = get_jwt_identity()
 
-#     if current_user.get("admin") == 1:
-#         data = request.get_json()
-#         room_name = data.get("room_name")
-#         status = data.get("status", 0)
+    if current_user == 1:
+        data = request.get_json()
+        room_name = data.get("room_name")
+        status = data.get("status", 0)
 
-#         conn = create_db_connection()
-#         cursor = conn.cursor()
-#         cursor.execute(
-#             "SELECT room_name FROM room_meeting WHERE room_name = %s", (
-#                 room_name,)
-#         )
-#         existing_room = cursor.fetchone()
+        # Thực hiện kiểm tra xem room_name đã tồn tại trong CSDL chưa
+        existing_room = Room.query.filter_by(room_name=room_name).first()
+        if existing_room:
+            return jsonify({"error": "Room already exists"}), 400
 
-#         if existing_room:
-#             return jsonify({"error": "Room already exists"}), 400
+        # Tạo phòng mới và lưu vào CSDL
+        new_room = Room(room_name=room_name, status=status)
+        db.session.add(new_room)
+        db.session.commit()
 
-#         cursor.execute(
-#             "INSERT INTO room_meeting (room_name, status) VALUES (%s, %s)",
-#             (room_name, status),
-#         )
-#         conn.commit()
-#         return jsonify({"message": "Room created successfully"})
-#     else:
-#         return jsonify({"error": "Permission denied"}), 403
+        return jsonify({"message": "Room created successfully"})
+    else:
+        return jsonify({"error": "Permission denied"}), 403
 
 
-# @app.route("/rooms/<int:room_id>", methods=["PUT"])
-# @jwt_required()
-# def update_room(room_id):
-#     current_user = get_jwt_identity()
+@app.route("/rooms/<int:room_id>", methods=["PUT"])
+@jwt_required()
+def update_room(room_id):
+    current_user = get_jwt_identity()
 
-#     if current_user.get("admin") == 1:
-#         try:
-#             data = request.get_json()
-#             room_name = data.get("room_name")
+    if current_user == 1:
+        data = request.get_json()
+        room_name = data.get("room_name")
 
-#             conn = create_db_connection()
-#             cursor = conn.cursor()
-#             cursor.execute(
-#                 "SELECT room_name FROM room_meeting WHERE room_id != %s AND room_name = %s",
-#                 (room_id, room_name),
-#             )
-#             existing_room = cursor.fetchone()
+        # Kiểm tra xem room_name đã tồn tại trong CSDL cho một phòng khác
+        existing_room = Room.query.filter(
+            Room.room_id != room_id, Room.room_name == room_name).first()
+        if existing_room:
+            return jsonify({"error": "Room name already exists"}), 400
 
-#             if existing_room:
-#                 return jsonify({"error": "Room name already exists"}), 400
+        # Tìm phòng cần cập nhật theo room_id
+        room_to_update = Room.query.get(room_id)
 
-#             cursor.execute(
-#                 "UPDATE room_meeting SET room_name=%s WHERE room_id=%s",
-#                 (room_name, room_id),
-#             )
-#             conn.commit()
-#             return jsonify({"message": "Room updated successfully"})
-#         except Exception as e:
-#             return jsonify({"error": "Internal Server Error"}), 500
-#         finally:
-#             cursor.close()
-#             conn.close()
-#     else:
-#         return jsonify({"error": "Permission denied"}), 403
+        if room_to_update:
+            # Cập nhật thông tin phòng và lưu vào CSDL
+            room_to_update.room_name = room_name
+            db.session.commit()
+            return jsonify({"message": "Room updated successfully"})
+        else:
+            return jsonify({"error": "Room not found"}), 404
+    else:
+        return jsonify({"error": "Permission denied"}), 403
 
 
-# @app.route("/rooms/<int:room_id>", methods=["DELETE"])
-# @jwt_required()
-# def delete_room(room_id):
-#     current_user = get_jwt_identity()
+@app.route("/rooms/<int:room_id>", methods=["DELETE"])
+@jwt_required()
+def delete_room(room_id):
+    current_user = get_jwt_identity()
 
-#     if current_user.get("admin") == 1:
-#         try:
-#             conn = create_db_connection()
-#             cursor = conn.cursor()
-#             cursor.execute(
-#                 "DELETE FROM room_meeting WHERE room_id=%s", (room_id,))
-#             conn.commit()
-#             return jsonify({"message": "Room deleted successfully"})
-#         except Exception as e:
-#             return jsonify({"error": "Internal Server Error"}), 500
-#         finally:
-#             cursor.close()
-#             conn.close()
-#     else:
-#         return jsonify({"error": "Permission denied"}), 403
+    if current_user.get == 1:
+        # Tìm phòng cần xóa theo room_id
+        room_to_delete = Room.query.get(room_id)
+
+        if room_to_delete:
+            # Xóa phòng khỏi CSDL và lưu thay đổi
+            db.session.delete(room_to_delete)
+            db.session.commit()
+            return jsonify({"message": "Room deleted successfully"})
+        else:
+            return jsonify({"error": "Room not found"}), 404
+    else:
+        return jsonify({"error": "Permission denied"}), 403
